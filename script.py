ac54d363116ea09df8e9e7bca463d9da2d2c0a6e
Vinayak Rathore <vinayakrathore441@gmail.com>
2026-04-09 18:44:03 +0530

    improve performance by DESKTOP-7BPHQFP at 2026-04-09 18:44:02.522811

--- script.py
+++ script.py
@@ -10,3 +10,6 @@ def is_even(n): return n%2==0
 
 # Update by DESKTOP-7BPHQFP at 2026-04-09 17:43:52.697298
 def greet(name): return f'Hello {name}'
+
+# Update by DESKTOP-7BPHQFP at 2026-04-09 18:44:01.765579
+def square(x): return x*x
