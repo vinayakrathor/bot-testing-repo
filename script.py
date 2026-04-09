7c6b74ad763ea2e4d37bf731d6598d1e9a96a1e2
Vinayak Rathore <vinayakrathore441@gmail.com>
2026-04-09 15:51:13 +0530

    update logic by DESKTOP-7BPHQFP at 2026-04-09 15:51:13.046547

--- script.py
+++ script.py
@@ -4,3 +4,6 @@ def add(a,b): return a+b
 
 # Update by DESKTOP-7BPHQFP at 2026-04-09 15:40:23.950853
 def greet(name): return f'Hello {name}'
+
+# Update by DESKTOP-7BPHQFP at 2026-04-09 15:51:12.807690
+def is_even(n): return n%2==0
