3b5a53ff7a31b598c1d735a07394e8b5bd69b4fe
Vinayak Rathore <vinayakrathore441@gmail.com>
2026-04-11 22:15:41 +0530

    update logic by DESKTOP-7BPHQFP at 2026-04-11 22:15:41.801896

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ function greet(name){return `Hello ${name}`;}
 
 // Update by DESKTOP-7BPHQFP at 2026-04-09 15:32:06.479955
 function add(a,b){return a+b;}
+
+// Update by DESKTOP-7BPHQFP at 2026-04-11 22:15:41.692611
+function square(x){return x*x;}
