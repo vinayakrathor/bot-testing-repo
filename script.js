3d41bb84c7274fdb46c7b440aefe5b276b6682df
Vinayak Rathore <vinayakrathore441@gmail.com>
2026-04-11 22:16:38 +0530

    refactor code by DESKTOP-7BPHQFP at 2026-04-11 22:16:38.650480

--- script.js
+++ script.js
@@ -16,3 +16,6 @@ function square(x){return x*x;}
 
 // Update by DESKTOP-7BPHQFP at 2026-04-11 22:16:19.319758
 function add(a,b){return a+b;}
+
+// Update by DESKTOP-7BPHQFP at 2026-04-11 22:16:38.575633
+function add(a,b){return a+b;}
