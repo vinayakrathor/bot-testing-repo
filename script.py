53716c7f07bb3c38e0ef8cb6a9f8baa873bc28d0
Vinayak Rathore <vinayakrathore441@gmail.com>
2026-04-20 19:53:30 +0530

    fix bug by DESKTOP-7BPHQFP at 2026-04-20 19:53:30.372541

--- script.py
+++ script.py
@@ -58,3 +58,6 @@ def is_even(n): return n%2==0
 
 # Update by DESKTOP-7BPHQFP at 2026-04-20 19:52:26.113127
 def square(x): return x*x
+
+# Update by DESKTOP-7BPHQFP at 2026-04-20 19:53:30.265268
+def add(a,b): return a+b
